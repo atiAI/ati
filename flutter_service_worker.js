91841bbdf1e81ace4ef8e556194d0d45b09a7bb9
VoltrY <75477566+VoltrY@users.noreply.github.com>
2024-11-03 12:53:45 +0000

    Deploying to gh-pages from @ atiAI/ati@0c18fdb8c9362135a511cf1ccd87c50fdad3da47 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "icons/Icon-maskable-512.png": "4c926986c6cb87b8133ba60c154c703d",
 "favicon.png": "f2f0bea894b63fc7df99d8d8b7e3ca83",
 "version.json": "615e64fb0651b738a87cf3c74598a4aa",
-"flutter_bootstrap.js": "8727722a12ec7239238f94074d71aafd"};
+"flutter_bootstrap.js": "6c9f34bfd942259acd8b1bb9180d5429"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
